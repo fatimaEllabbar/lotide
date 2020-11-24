87469e9898689ddb878a3d433470e631edba7866
vagrant <fatima.ellabbar@gmail.com>
2020-11-24 23:51:51 +0000

    add countOnly

---
+++ countOnly.js
@@ -0,0 +1,47 @@
+const assertEqual = function(actual, expected) {
+  let quote1 = "";
+  let quote2= "";
+  if ( isNaN(actual)){
+      quote1 = "\"";
+  } 
+  if ( isNaN(expected)){
+    quote2 = "\"";
+  } 
+  if( actual === expected) {
+    console.log(`${String.fromCodePoint(0x2705)}${String.fromCodePoint(0x2705)}${String.fromCodePoint(0x2705)}Assertion Passed: ${quote1}${actual}${quote1} === ${quote2}${expected}${quote2}`);
+  } else {
+    console.log(`${String.fromCodePoint(0x274C)}${String.fromCodePoint(0x274C)}${String.fromCodePoint(0x274C)}Assertion Failed: ${quote1}${actual}${quote1} !== ${quote2}${expected}${quote2}`)
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  let results= {}
+  for (let item in itemsToCount){
+    if ( itemsToCount[item] === true){
+      for ( const element in allItems){
+        if(item === allItems[element]){
+          if (results[item]) {
+            results[item] += 1;
+          } else {
+            results[item] = 1;
+          }
+        }
+      }
+    } 
+    } 
+    return results
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+console.log(countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false }));
