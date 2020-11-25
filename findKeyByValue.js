eddf19a735c52cf399e4c5137feed4348f2f21a2
vagrant <fatima.ellabbar@gmail.com>
2020-11-25 00:42:36 +0000

    adding find key by value

---
+++ findKeyByValue.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  let quote1 = "";
+  let quote2= "";
+  if ( isNaN(actual)){
+      quote1 = "\"";
+  } 
+  if ( isNaN(expected)){
+    quote2 = "\"";
+  } 
+  if( actual === expected) {
+    console.log(`${String.fromCodePoint(0x2705)}${String.fromCodePoint(0x2705)}${String.fromCodePoint(0x2705)}Assertion Passed: ${quote1}${actual}${quote1} === ${quote2}${expected}${quote2}`);
+  } else {
+    console.log(`${String.fromCodePoint(0x274C)}${String.fromCodePoint(0x274C)}${String.fromCodePoint(0x274C)}Assertion Failed: ${quote1}${actual}${quote1} !== ${quote2}${expected}${quote2}`)
+  }
+};
+
+const findKeyByValue = function (obj, value){
+  for (const key of Object.keys(obj)){
+    if (obj[key] === value)
+    return value;
+  }
+  return undefined;
+}
+
+
+ const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
